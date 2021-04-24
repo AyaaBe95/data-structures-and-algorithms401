444b4ec18ac773ed342cd9445e38d912b2119b17
aya berjawi <ayabarjawi@gmail.com>
2021-04-24 16:38:25 +0300

    lab12

--- Data-Structures/stack-and-queue/lib/src/main/java/stack/and/queue/Library.java
+++ Data-Structures/stack-and-queue/lib/src/main/java/stack/and/queue/Library.java
@@ -7,27 +7,33 @@ public class Library {
 
     public static void main(String[] args) {
 
-        stack stack = new stack();
-        System.out.println(stack.isEmpty());
-        stack.push(5);
-        stack.push(2);
-        stack.push(3);
-        System.out.println(stack.peek());
+//        stack stack = new stack();
+//        System.out.println(stack.isEmpty());
+//        stack.push(5);
+//        stack.push(2);
+//        stack.push(3);
+//        System.out.println(stack.peek());
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//
+//        queue queue = new queue();
+//        queue.enqueue(2);
+//        System.out.println(queue);
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue);
+//        System.out.println(queue.isEmpty());
+//        System.out.println(queue.peek());
+//        queue.enqueue(2);
+//
+//        System.out.println(queue.dequeue());
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        AnimalShelter a = new AnimalShelter();
+        a.enqueue("caaat");
 
-        queue queue = new queue();
-        queue.enqueue(2);
-        System.out.println(queue);
-        System.out.println(queue.dequeue());
-        System.out.println(queue);
-        System.out.println(queue.isEmpty());
-        System.out.println(queue.peek());
-        queue.enqueue(2);
 
-        System.out.println(queue.dequeue());
+
 
 
     }
