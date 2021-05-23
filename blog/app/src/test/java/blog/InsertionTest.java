cbf7722e2ea60383078b9c7ff41c0da9855d69e3
aya berjawi <ayabarjawi@gmail.com>
2021-05-23 12:41:40 +0300

    fixed

--- blog/app/src/test/java/blog/InsertionTest.java
+++ blog/app/src/test/java/blog/InsertionTest.java
@@ -1,6 +1,8 @@
 package blog;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,22 +11,24 @@ public class InsertionTest {
     @Test
     public void testSorting1() {
         int[] arr = new int[] {8,4,23,42,16,15};
-        assertEquals("[4, 8, 15, 16, 23, 42]", insertionSort.sorting(arr));
+        assertArrayEquals(new int[]{4, 8, 15, 16, 23, 42},insertionSort.sorting(arr));
     }
 
     @Test public void testSorting2() {
         int[] arr = new int[] {20,18,12,8,5,-2};
-        assertEquals("[-2, 5, 8, 12, 18, 20]", insertionSort.sorting(arr));
+        assertArrayEquals(new int[]{-2, 5, 8, 12, 18, 20},insertionSort.sorting(arr));
+
     }
 
     @Test public void testSorting3() {
         int[] arr = new int[] {2,3,5,7,13,11};
-        assertEquals("[2, 3, 5, 7, 11, 13]", insertionSort.sorting(arr));
+        assertArrayEquals(new int[]{2, 3, 5, 7, 11, 13},insertionSort.sorting(arr));
     }
 
     @Test public void testSorting4() {
         int[] arr = new int[] {5,12,7,5,5,7};
-        assertEquals("[5, 5, 5, 7, 7, 12]", insertionSort.sorting(arr));
+        assertArrayEquals(new int[]{5, 5, 5, 7, 7, 12},insertionSort.sorting(arr));
+
     }
 
 
