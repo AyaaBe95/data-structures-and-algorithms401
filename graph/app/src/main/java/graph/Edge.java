23981f40fb051a81e8257935d410285dd7e1b0e8
aya berjawi <ayabarjawi@gmail.com>
2021-05-26 15:55:51 +0300

    graph

---
+++ graph/app/src/main/java/graph/Edge.java
@@ -0,0 +1,41 @@
+package graph;
+
+public class Edge {
+
+   private Vertex vertex;
+   private int weight;
+
+   public Edge(Vertex vertex,int weight){
+       this.vertex=vertex;
+       this.weight=weight;
+   }
+
+   public Edge(Vertex vertex){
+       this.vertex=vertex;
+       this.weight=0;
+   }
+
+    public Vertex getVertex() {
+        return vertex;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setVertex(Vertex vertex) {
+        this.vertex = vertex;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "vertex=" + vertex +
+                ", weight=" + weight +
+                '}';
+    }
+}
